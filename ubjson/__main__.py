b4ce78449bf7232e65c581d78d59ec30520c3858
Vilnis Termanis <vilnis.termanis@iotic-labs.com>
2016-02-16 18:58:38 +0000

    License link mistake correction

--- ubjson/__main__.py
+++ ubjson/__main__.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://github.com/Iotic-Labs/py-lz4framed/blob/master/LICENSE
+#     https://github.com/Iotic-Labs/py-ubjson/blob/master/LICENSE
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
